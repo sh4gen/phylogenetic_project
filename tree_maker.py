035bf8e7435ac7bbb90b1105c6b875dcf4e7224e
shagen <keylogershagen@gmail.com>
2024-04-20 23:23:55 +0300

    fixes and additions.

--- tree_maker.py
+++ tree_maker.py
@@ -1,3 +1,4 @@
+from graphviz import Digraph
 class Node:
     def __init__(self, value: str, info: str = "", parent=None) -> None:
         self.value = value
@@ -19,19 +20,17 @@ def parse_taxonomy_data(data):
         
         parent_values = parts[2].strip().split(';')
         parent_values = [pv.strip() for pv in parent_values if pv.strip()]
-
+    
         node = Node(value, info)
-        node_dict[value] = node
+        node_dict[info] = node
 
-        # Ebeveyn düğümü bul ve çocuk olarak ekle
         if parent_values:
-            parent_value = parent_values[-1]  # En son ebeveyni al
+            parent_value = parent_values[-1]
             parent_node = node_dict.get(parent_value)
             if parent_node:
                 node.parent = parent_node
                 parent_node.add_child(node)
 
-    # Kök düğümü bul
     root = None
     for node in node_dict.values():
         if node.parent is None:
@@ -40,26 +39,32 @@ def parse_taxonomy_data(data):
 
     return root
 
-
 def print_tree(node, depth=0):
     if node is None:
         return
-    print("  " * depth + node.value)
+    print("  " * depth + node.info)
     for child in node.children:
         print_tree(child, depth + 1)
 
-data =  """
-        2	|	2	|		|
-        7	|	7	|	2;	|
-        5	|	5	|	2;	|
-        12	|	12	|	2; 7;	|
-        10	|	10	|	2; 7;	|
-        6	|	6	|	2; 7;	|
-        9	|	9	|	2; 5;	|
-        15	|	15	|	2; 7; 6;	|
-        11	|	11	|	2; 7; 6;	|
-        4	|	4	|	2; 5; 9;	|
-        """
+def generate_dot(node, dot, parent_id=None):
+    if node is None:
+        return
+    
+    node_id = node.value
+    dot.node(node_id, node.info)
+    
+    if parent_id:
+        dot.edge(parent_id, node_id)
+
+    for child in node.children:
+        generate_dot(child, dot, node_id)
+
+with open('ver.txt') as f:
+    data = f.read()
 
 root_node = parse_taxonomy_data(data)
 print_tree(root_node)
+
+dot = Digraph(comment='Taxonomy Tree', graph_attr={'size': '1920,1080'})
+generate_dot(root_node, dot)
+dot.render('taxonomy_tree', format='png', cleanup=True)
